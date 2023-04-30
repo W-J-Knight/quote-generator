8809961c69675cc780467b6ba9f23748c16d99bf
William Knight <William.Jeffery.Knight@protonmail.com>
2023-04-30 08:41:47 -0400

    console log one randon quote

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("testing");
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+  // Create a random number
+  const randNum = Math.floor(Math.random() * apiQuotes.length);
+  // Pick a random quote from apiQuote array
+  const quote = apiQuotes[randNum];
+  console.log(quote);
+}
+
+// Get Quotes From API
+async function getQuotes() {
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (err) {
+    // Catch error here
+    console.log(`ERROR: ${err}`);
+  }
+}
+
+//ON Load
+getQuotes();
